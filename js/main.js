d350b0f8316418b223fd0f21047368772b420b7d
Ethan McFarlin <ethanmcfarlin@gmail.com>
2018-01-26 22:24:18 -0700

    Twitter feed

--- js/main.js
+++ js/main.js
@@ -148,4 +148,77 @@ if (!demo4.error) {
   console.error(demo4.error);
 }
 
+// Timeline
 
+jQuery(document).ready(function($){
+	var contentSections = $('.cd-section'),
+		navigationItems = $('#cd-vertical-nav a');
+
+	updateNavigation();
+	$(window).on('scroll', function(){
+		updateNavigation();
+	});
+
+	//smooth scroll to the section
+	navigationItems.on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+    //smooth scroll to second section
+    $('.cd-scroll-down').on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+
+    //open-close navigation on touch devices
+    $('.touch .cd-nav-trigger').on('click', function(){
+    	$('.touch #cd-vertical-nav').toggleClass('open');
+  
+    });
+    //close navigation on touch devices when selectin an elemnt from the list
+    $('.touch #cd-vertical-nav a').on('click', function(){
+    	$('.touch #cd-vertical-nav').removeClass('open');
+    });
+
+	function updateNavigation() {
+		contentSections.each(function(){
+			$this = $(this);
+			var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+				navigationItems.eq(activeSection).addClass('is-selected');
+			}else {
+				navigationItems.eq(activeSection).removeClass('is-selected');
+			}
+		});
+	}
+
+	function smoothScroll(target) {
+        $('body,html').animate(
+        	{'scrollTop':target.offset().top},
+        	600
+        );
+	}
+});
+
+// Twitter Feed
+
+//Follow Button Effect
+
+$(document).ready(
+
+	function iniciar(){
+	$('#doherty-follow').on("click", function(){
+		$('#doherty-follow').css('background-color','#34CF7A');
+		$('#doherty-follow').html('<div class="icon-ok"></div> Following');
+	});	
+	$('#pltw-follow').on("click", function(){
+		$('#pltw-follow').css('background-color','#34CF7A');
+		$('#pltw-follow').html('<div class="icon-ok"></div> Following');
+	});	
+	$('#d11-follow').on("click", function(){
+		$('#d11-follow').css('background-color','#34CF7A');
+		$('#d11-follow').html('<div class="icon-ok"></div> Following');
+	});	
+	}
+
+);
